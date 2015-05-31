c1dcf6b31b102c92a8894b5d1e4df5cccaef333f
ganesh <ganeshbevara10@gmail.com>
2015-05-31 15:32:33 -0400

    this is my first commit ...yay...

---
+++ rest/src/com/rest/service/SampleService.java
@@ -0,0 +1,16 @@
+package com.rest.service;
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+@Path("/sampleService")
+public class SampleService {
+	
+	@GET
+	@Path("/{name}")
+	public String generateMessage(@PathParam("name") String name){
+		return "this is rest service: "+name;
+	}
+}
